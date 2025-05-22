86a5ec30a73210c9df8eebf3c61ab7aa2ff48fab
shivam-231093 <shivam.mishra.231093@gmail.com>
2025-05-22 14:53:37 +0530

    final6

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Investors from './pages/Investors';
 import Contact from './pages/Contact';
 import ScrollToTop from './components/ScrollToTop';
 import Footer from './pages/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <Router>
       <ScrollToTop />
       <div className='w-full relative min-h-screen flex flex-col'>
+       <Analytics />
         
         <div className='relative z-10'>
           <Nav />
